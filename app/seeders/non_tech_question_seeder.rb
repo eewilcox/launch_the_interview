d6697f8a48ef609f91c9ec302e40b12e62b180a5
Emma Wilcox <icecubesky@gmail.com>
2017-04-04 11:01:23 -0400

    seeders

---
+++ app/seeders/non_tech_question_seeder.rb
@@ -0,0 +1,24 @@
+class NonTechQuestionSeeder
+  NONTECH = [
+    "Why are you here interviewing with us?",
+    "Why the career switch into development?",
+    "Talk about your experience of working in a team.",
+    "How do you manage conflicts when working in a group?",
+    "What is your dream job?",
+    "What is your goal for the next 5 years?",
+    "Why did you decide to study web development?",
+    "What is your favorite programming language? Why?",
+    "What piece of code are you most proud of?",
+    "What did you learn yesterday?",
+    "What UI, security, performance, SEO, maintainability or technology considerations do you make while building a web application or site?",
+    "If you could master one technology this year, what would it be?",
+    "What industry sites and blogs do you read regularly?",
+    "If you were hiring for a junior web developer position, what qualities would you look for?"
+  ]
+
+  def self.seed!
+    NONTECH.each do |question|
+      Question.find_or_create_by!(body: question, category_id: "1")
+    end
+  end
+end
