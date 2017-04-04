4b7e05b9734cdeda816e03098f79917ceb07dccc
Emma Wilcox <icecubesky@gmail.com>
2017-04-04 11:26:00 -0400

    more styling

--- react/src/components/QuestionTile.js
+++ react/src/components/QuestionTile.js
@@ -3,7 +3,7 @@ import React from 'react';
 const QuestionTile = (props) => {
     return(
       <div onClick={props.toggleAnswer} className="question">
-        <h5>
+        <h5 className="question-tiles">
           <span><i className="fa fa-question-circle-o" aria-hidden="true"></i></span>
           {props.body}
         </h5>
